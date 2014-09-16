d0e86afead79816c6b1ad05f8eed8228af6dbb39
Thomas Nelson <thomas.nelson@colorado.edu>
2014-09-15 19:12:31 -0600

    build system and first prototypes

--- build.sbt
+++ build.sbt
@@ -2,6 +2,8 @@ name := "OCTOPI"
 
 version := "0.1"
 
-scalaVersion := "2.11.1"
+scalaVersion := "2.11.2"
 
-libraryDependencies += "com.googlecode.kiama" %% "kiama" % "1.6.0"
+libraryDependencies += "com.googlecode.kiama" %% "kiama" % "1.7.0"
+
+scalacOptions ++= Seq("-unchecked", "-deprecation")
--- octopi.scala
+++ octopi.scala
@@ -3,23 +3,36 @@ import org.kiama.output.PrettyPrinter
 import org.kiama.attribution.Attribution._
 import org.kiama.attribution.Attribution
 import org.kiama.attribution.Attributable
+import java.io._
+import scala.collection.immutable.Seq
 
 object OctoTree {
 	import org.kiama.util.TreeNode
 
-	abstract class ONode extends TreeNode
+	sealed trait OctoTree extends TreeNode  // the Octopi Node type for everything
 
-    case class Tensor(name: String, ind: List[String]) extends ONode
 
-	case class Mul(lhs: ONode, rhs: ONode) extends ONode
+	sealed abstract class Exp extends OctoTree
 
-	case class Sum(lhs: ONode, ind: List[String], rhs: ONode) extends ONode
+	case class Tensor(name: String, ind: Seq[String]) extends Exp
 
-	case class Program(decl: List[Tensor], code: ONode) extends ONode
+	case class Mul(lhs: Exp, rhs: Exp) extends Exp
 
-	case class Addr(indx: String, sub: Option[Addr]) extends ONode
 
-	case class IndTensor(name: String, ind: String) extends ONode
+	sealed abstract class Stmt extends OctoTree
+
+	// eventually bounds should be something else
+	case class Loop(body: Stmt, ivar: String, bounds: String) extends Stmt 
+
+	case class Assign(lhs: Tensor, rhs: Exp) extends Stmt // lhs = rhs
+
+
+	// Later we will expand on this, at the very least a list of statements
+	sealed abstract class Program extends OctoTree
+
+	case class Sum(decl: Seq[Tensor], lhs: Tensor, ind: Seq[String], rhs: Exp) extends Program
+
+	case class Code(decl: Seq[Tensor], stmt: Stmt) extends Program
 
 	// Each index is actually the size of that dimension
 	def containerSizes(t: Tensor) : (String, List[String]) = {
@@ -42,56 +55,72 @@ trait SyntaxAnalyser extends JavaTokenParsers {
     def tensor : Parser[Tensor] =
         ident ~ indices ^^ {case n ~ i => Tensor(n,i)}
 
-	def term : Parser[ONode] = 
+	def term : Parser[Exp] = 
 		factor ~ opt( "*" ~ factor ) ^^ //(x => x match
 			{
 				case l ~ Some("*" ~ r) => Mul(l,r)
 				case l ~ None => l
 			}
 	
-	def factor : Parser[ONode] = 
+	def factor : Parser[Exp] = 
 		tensor | 
 		"(" ~ term ~ ")" ^^ {case "(" ~ t ~ ")" => t}
 
+	/*
 	def stmt : Parser[Sum] = 
 		tensor ~ "= sum(" ~ indices ~ "," ~ term <~ ")" ^^ 
 			{case lhs ~ "= sum(" ~ i ~ "," ~ t => Sum(lhs, i, t)}
+	*/
 	
-	// Top Level Program
-	def program : Parser[Program] = 
-		rep(tensor) ~ "{" ~ stmt <~ "}" ^^ {case d ~ "{" ~ s => Program(d, s)}
+	// Top Level Program = a single sum (for now)
+	def program : Parser[Sum] = 
+		rep(tensor) ~ "{" ~ tensor ~ "= sum(" ~ indices ~ "," ~ term <~ ")" <~ "}" ^^ 
+			{case d ~ "{" ~ lhs ~ "= sum(" ~ ind ~ "," ~ rhs => Sum(d, lhs, ind, rhs)}
 }
 
 object CPrinter extends PrettyPrinter {
 	import OctoTree._
 
-	def pretty(t : ONode) : String = {
+	def pretty(t : OctoTree) : String = {
 		super.pretty(show(t))
 	}
 	
-	def show(t : ONode) : Doc = {
+	def show(t : OctoTree) : Doc = {
+
 		t match {
-			case Tensor(name,ind) => name <> "[" <> sep(ind map text) <> "]"
-			case IndTensor(name,ind) => name <> "[" <> ind  <> "]"
+			case Tensor(name,ind) => name <> "[" <> flattenAccess(ind, "") <> "]"
 			case Mul(lhs, rhs) => show(lhs) <> "*" <> show(rhs)
-			case Sum(lhs, ind, rhs) => {
-				// val loopIndices = lhs.ind ::: ind
-				return sep(ind.map(indexLoop) map text) <@>
-					show(lhs) <+> "=" <+> show(rhs) <@> 
-					sep(ind.map(_ => "}") map text)
+			case Sum(decl, lhs, ind, rhs) => {
+				"Declarations: " <> sep(decl.map(show)) <+> show(lhs) <> "= Sum(" <+>  ssep(ind map text, "][") <> "," <> show(rhs) <> ")"
 			}
-			case Program(decl, code) => 
-				("void axpy(double* "+ decl.mkString(", double* ") + ") {\n") <> show(code) <> "\n}"
+
+			case Assign(lhs, rhs) => show(lhs) <> "=" <> show(rhs)
+
+			case Loop(body, ivar, bounds) => "for (" <> bounds <> ") {" <@> show(body)  <@> "}"
+
+			case Code(decl: Seq[Tensor], stmt: Stmt) => "void nek(double *" + functionArgs(decl) + ") {" <@> show(stmt) <@> "}"
+
+			// case _ => throw new RuntimeException("unrecognized case in PrettyPrint")
+			/*
 			case Addr(indx, sub) => sub match {
 				case None => indx
 				case Some(a) => indx <+> show(a)
 			}
+			*/
 		}
 	}
 
 	def indexLoop(i:String) = "for (int "+i+"=0; " + i + "<10; ++" + i + ") {"
+
+	def functionArgs(vars: Seq[Tensor]) = vars.map(_.name).mkString(", double* ")
+
+	def flattenAccess(ind: Seq[String], stride: String) : String = ind match {
+		case Seq() => return ""
+		case x :: xs => x + stride + " + " + flattenAccess(xs, stride + "*" + x + "_max")
+	}
 }
 
+/*
 trait Evaluator {
 	import OctoTree._
 	import org.kiama.rewriting.Rewriter._
@@ -107,8 +136,28 @@ trait Evaluator {
 	val change_all_tensors = topdown(tensor_rewrite)
 
 }
+*/
 
+trait Evaluator {
+	import OctoTree._
 
+	def express(s: Sum) : Code = {
+		s match {
+			case Sum(decl, lhs, ind, rhs) => 
+				Code(decl, lowerLoops(ind ++ lhs.ind, lhs, rhs))
+			// case _ => throw new RuntimeException("unrecognized case in express")
+		}
+	}
+
+	def lowerLoops(indexList : Seq[String], lhs: Tensor, rhs: Exp) : Stmt = {
+		indexList match {
+			case Seq() => Assign(lhs, rhs)
+			case x :: xs => Loop(lowerLoops(xs, lhs, rhs), x, "int " + x + " = 0; " + x + " < 10; ++" + x)
+		}
+	}
+}
+
+// object OCTOPI extends SyntaxAnalyser with Evaluator {
 object OCTOPI extends SyntaxAnalyser with Evaluator {
 
 	import OctoTree._
@@ -122,13 +171,22 @@ object OCTOPI extends SyntaxAnalyser with Evaluator {
 		val parser_result = parseAll(program, reader)
 		parser_result match {
 			case Success(program,i) => {
-				println(program.decl.map(OctoTree.containerSizes))
-				// rewrite tries to apply the rewrite rule, if it fails you get the original term back
-				val s = rewrite[ONode](change_all_tensors)(program.code)
-				println(program.code)
-				println(s)
-				println(CPrinter.pretty(program.code))
-				println(CPrinter.pretty(s))
+				println(program.decl.map(OctoTree.containerSizes _))
+				// rewrite tries to apply the rewrite rule, 
+				// if it fails you get the original term back
+				// val s = rewrite[ONode](change_all_tensors)(program.code)
+
+				println("==  Before Lowering ==")
+				println(program)
+				println(CPrinter.pretty(program))
+				val lowered = express(program)
+				println("== After Lowering ==")
+				println(lowered)
+				println(CPrinter.pretty(lowered))
+				val output = new FileWriter("tuned.c")
+				output.write(CPrinter.pretty(lowered))
+				output.close()
+				println("== wrote file to tuned.c, compile and test! ==")
 			}
 			case Failure(m,i) => println(m)
 			case Error(m,i) => println(m)
